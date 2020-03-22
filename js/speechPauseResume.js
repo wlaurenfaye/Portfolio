f602845f9b0f23a5dba66623d7fc598099dc5d4b
Lauren <wlaurenfaye@gmail.com>
2020-03-22 23:20:37 +0000

    coursework submit

---
+++ js/speechPauseResume.js
@@ -0,0 +1,17 @@
+
+  function speak(obj) {
+  $(obj).articulate('speak');
+  };
+
+  function pause() {
+    $().articulate('pause');
+  };
+
+  function resume() {
+    $().articulate('resume');
+  };
+
+  function stop() {
+    $().articulate('stop');
+  };
+ 
