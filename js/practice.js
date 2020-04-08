1be95a6fe6254ff32b36a3e31b408565211a7b6e
Lauren <wlaurenfaye@gmail.com>
2020-04-08 20:41:50 +0100

    js project

--- js/practice.js
+++ js/practice.js
@@ -1,6 +1,13 @@
-function
+const colorBtn = document.querySelector('colorBtn');
+const bodyBcd = document.querySelector('body');
 
 
-myFunction() {
-document.body.style.background="pink";
+const colors = ['yellow', 'red', 'green', '#3b5998'];
+
+colorBtn.addEventListener("click", changeColor());
+
+
+
+function changeColor () {
+  bodyBcg.style.backgroundColor = 'blue';
 }
